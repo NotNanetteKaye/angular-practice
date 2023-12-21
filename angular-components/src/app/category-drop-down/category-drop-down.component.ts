504eb1a51cda06f8bcf2c0dd012a9bbf0583c3c3
Nanette Kaye Dolera <106824983+NotNanetteKaye@users.noreply.github.com>
2023-12-21 11:55:47 -0600

    Added component to app.html | added dummy data to dropdown.component.ts

--- angular-components/src/app/category-drop-down/category-drop-down.component.ts
+++ angular-components/src/app/category-drop-down/category-drop-down.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./category-drop-down.component.css']
 })
 export class CategoryDropDownComponent {
-
+  // Dummy data for the dropdown
+  categories: string[] = ['Category 1', 'Category 2', 'Category 3', 'Category 4'];
+  selectedCategory: string | undefined;
 }
